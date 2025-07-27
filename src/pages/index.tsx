11cbb69ce3f75501d8929d59eed4fae4d01faf90
Riley Griffin <RGriffin@connecture.com>
2025-07-26 22:36:24 -0700

    Removed unused import.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "@heroui/link";
 import { button as buttonStyles } from "@heroui/theme";
 import { title, subtitle } from "@/components/primitives";
 import GradientBackgroundLayout from "@/layouts/gradientBackgroundLayout";
-import { navLinks, subPageLinks } from "@/config/site";
+import { navLinks } from "@/config/site";
 
 //----------------------------------------------------------------------------------------
 //Create Component
