d2b5a320213f9cd982c214823ce44da4f5f81298
Riley Griffin <RGriffin@connecture.com>
2025-08-29 18:09:33 -0700

    Added mobile support for Dice Roll.

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -152,6 +152,9 @@ export const splitOnLineBreak = (input: string): string[] =>
 export const removeAllSpaces = (input: string): string =>
 	input.replace(/ /g, "");
 
+export const replaceAllLineBreaksWithComma = (input: string): string =>
+	input.replace(lineBreakRegexGlobal, ",");
+
 export const escapeAllSingleQuotes = (input: string): string =>
 	input.replace(/'/g, "''");
 
